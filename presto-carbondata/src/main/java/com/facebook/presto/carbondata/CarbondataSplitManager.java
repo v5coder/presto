0338981328aab294cc057b908e55c11b0a00de05
v5uncode <v5uncode@hotmail.com>
2018-09-14 15:16:11 +0800

    carbondata connection

---
+++ presto-carbondata/src/main/java/com/facebook/presto/carbondata/CarbondataSplitManager.java
@@ -0,0 +1,123 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.facebook.presto.carbondata;
+
+import com.facebook.presto.carbondata.impl.CarbonLocalMultiBlockSplit;
+import com.facebook.presto.carbondata.impl.CarbonTableCacheModel;
+import com.facebook.presto.carbondata.impl.CarbonTableReader;
+import com.facebook.presto.spi.*;
+import com.facebook.presto.spi.connector.ConnectorSplitManager;
+import com.facebook.presto.spi.connector.ConnectorTransactionHandle;
+import com.facebook.presto.spi.predicate.TupleDomain;
+import com.google.common.collect.ImmutableList;
+import org.apache.carbondata.core.scan.expression.Expression;
+import org.apache.carbondata.core.stats.QueryStatistic;
+import org.apache.carbondata.core.stats.QueryStatisticsConstants;
+import org.apache.carbondata.core.stats.QueryStatisticsRecorder;
+import org.apache.carbondata.core.util.CarbonTimeStatisticsFactory;
+
+import javax.inject.Inject;
+import java.util.List;
+import java.util.Optional;
+
+import static com.facebook.presto.carbondata.Types.checkType;
+import static java.util.Objects.requireNonNull;
+
+/**
+ * Build Carbontable splits
+ * filtering irrelevant blocks
+ */
+public class CarbondataSplitManager implements ConnectorSplitManager {
+
+    private final String connectorId;
+    private final CarbonTableReader carbonTableReader;
+
+    @Inject
+    public CarbondataSplitManager(CarbondataConnectorId connectorId, CarbonTableReader reader) {
+        this.connectorId = requireNonNull(connectorId, "connectorId is null").toString();
+        this.carbonTableReader = requireNonNull(reader, "client is null");
+    }
+
+    public ConnectorSplitSource getSplits(ConnectorTransactionHandle transactionHandle,
+                                          ConnectorSession session, ConnectorTableLayoutHandle layout) {
+        CarbondataTableLayoutHandle layoutHandle = (CarbondataTableLayoutHandle) layout;
+        CarbondataTableHandle tableHandle = layoutHandle.getTable();
+        SchemaTableName key = tableHandle.getSchemaTableName();
+
+        String queryId = System.nanoTime() + "";
+        QueryStatistic statistic = new QueryStatistic();
+        QueryStatisticsRecorder statisticRecorder = CarbonTimeStatisticsFactory.createDriverRecorder();
+        statistic.addStatistics(QueryStatisticsConstants.BLOCK_ALLOCATION, System.currentTimeMillis());
+        statisticRecorder.recordStatisticsForDriver(statistic, queryId);
+        statistic = new QueryStatistic();
+
+        carbonTableReader.setQueryId(queryId);
+        // Packaging presto-TupleDomain into CarbondataColumnConstraint,
+        // to decouple from presto-spi Module
+        List<CarbondataColumnConstraint> rebuildConstraints =
+                getColumnConstraints(layoutHandle.getConstraint());
+
+        CarbonTableCacheModel cache = carbonTableReader.getCarbonCache(key);
+        if (null != cache) {
+            Expression filters = PrestoFilterUtil.parseFilterExpression(layoutHandle.getConstraint());
+            try {
+                List<CarbonLocalMultiBlockSplit> splits =
+                        carbonTableReader.getInputSplits2(cache, filters, layoutHandle.getConstraint());
+
+                ImmutableList.Builder<ConnectorSplit> cSplits = ImmutableList.builder();
+                long index = 0;
+                for (CarbonLocalMultiBlockSplit split : splits) {
+                    index++;
+                    cSplits.add(new CarbondataSplit(connectorId, tableHandle.getSchemaTableName(),
+                            layoutHandle.getConstraint(), split, rebuildConstraints, queryId, index));
+                }
+
+                statisticRecorder.logStatisticsAsTableDriver();
+
+                statistic.addStatistics(QueryStatisticsConstants.BLOCK_IDENTIFICATION,
+                        System.currentTimeMillis());
+                statisticRecorder.recordStatisticsForDriver(statistic, queryId);
+                statisticRecorder.logStatisticsAsTableDriver();
+                return new FixedSplitSource(cSplits.build());
+            } catch (Exception ex) {
+                throw new RuntimeException(ex.getMessage(), ex);
+            }
+        }
+        return null;
+    }
+
+    /**
+     * @param constraint
+     * @return
+     */
+    public List<CarbondataColumnConstraint> getColumnConstraints(
+            TupleDomain<ColumnHandle> constraint) {
+        ImmutableList.Builder<CarbondataColumnConstraint> constraintBuilder = ImmutableList.builder();
+        for (TupleDomain.ColumnDomain<ColumnHandle> columnDomain : constraint.getColumnDomains()
+                .get()) {
+            CarbondataColumnHandle columnHandle =
+                    checkType(columnDomain.getColumn(), CarbondataColumnHandle.class, "column handle");
+
+            constraintBuilder.add(new CarbondataColumnConstraint(columnHandle.getColumnName(),
+                    Optional.of(columnDomain.getDomain()), columnHandle.isInvertedIndex()));
+        }
+
+        return constraintBuilder.build();
+    }
+
+}
